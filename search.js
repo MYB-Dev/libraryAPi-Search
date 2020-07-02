f3b0ef6ec5bd3befb782f75f0111212701661e89
MYB-Dev <mybdev20@gmail.com>
2020-07-02 02:44:17 +0100

    first commit

---
+++ search.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    type: '',
+    search: '',
+    books: [],
+  },
+  methods: {
+    submit(e) {
+      try {
+        fetch(
+          `https://myb-library-api.herokuapp.com/api/books/search?${this.type}=${this.search}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            this.books = data;
+          });
+      } catch (err) {
+        console.log(err);
+      }
+      this.search = '';
+      this.type = '';
+      e.preventDefault();
+    },
+  },
+});
